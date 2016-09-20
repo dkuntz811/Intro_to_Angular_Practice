29cdd9ecc00ce6a49ba77e9efac17b36240e59c8
Dave Kuntz <dkuntz811@gmail.com>
2016-09-20 14:16:55 -0500

    ngModel and ngClick Working

--- server/public/scripts/angular.js
+++ server/public/scripts/angular.js
@@ -7,4 +7,9 @@ var allCats=[];
 myApp.controller('catController', ['$scope', '$http', function($scope, $http){
 	console.log('NG');
 
+	var newCat = {
+		name: $scope.catName,
+		trait: $scope.catTrait
+	};//end newCat
+
 }]);//end controller
